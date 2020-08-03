b7ab17045c3b0de5a1de7a202e544c9cb339b32f
VENKATALAKSHMI SUBRAMANIAM <68234899+VenkatalakshmiS@users.noreply.github.com>
2020-08-03 21:27:24 +0530

    BASIC OPERATIONS IN PYTHON

---
+++ cyberfrat day 2.py
@@ -0,0 +1,93 @@
+from macpath import sep
+
+print(2+3)
+
+print(44*3)
+
+print(35/5)
+
+print(67-26)
+
+print(4**2)
+
+print(5**5)
+
+print(3/5)
+
+print(2.0/3)
+
+print(3.0/5)
+
+print(22%3)
+
+print(4%5)
+
+print(2//3)
+
+print(2.0//3)
+
+print("hello world")
+
+input("Enter a value:")
+
+data=[1,2,3,4,5]
+test=data[1:2]
+print(test)
+
+test=data
+print(test)
+
+n1=input("Enter a value")
+print(n1)
+
+n2=input("Enter another value")
+ans=n1+n2
+print(ans)
+
+
+n1=int(input("Enter a number"))
+n2=int(input("Enter a number"))
+ans=n1+n2
+print(ans)
+
+firstname="venkata"
+lastname="lakshmi"
+print(firstname,lastname)
+
+#print(firstname,lastname,sep="")
+
+#print(firstname,lastname,sep="*")
+
+#print(firstname,lastname,end=" ")
+
+print("Hello /n world")
+print("Hello/nworld")
+print("Hello/n world")
+
+
+print("{}##{}$${}".format("Venkata","Lakshmi","Python"))
+
+print("%d"%5)
+
+print("%3d"%5)
+
+print("%03d"%5)
+
+print("%3d"%3487)
+
+print("%08.3f"%53.67)
+
+print("%8.4f"%53.67)
+
+print("%08.4f"%53.67)
+
+print("##{}".format("%6.3f"%53.67))
+
+print("%06.5f"%15.89)
+
+print("%0.5f"%15.89)
+
+print("%03.5f"%15.89)
+
+
+
