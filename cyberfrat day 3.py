631ffa99bb42cbe1512008452faad5b17ceae028
VENKATALAKSHMI SUBRAMANIAM <68234899+VenkatalakshmiS@users.noreply.github.com>
2020-08-04 08:46:44 +0530

    CONDITIONAL AND LOOPING STATEMENTS IN PYTHON

---
+++ cyberfrat day 3.py
@@ -0,0 +1,83 @@
+age=int(input("Enter your age:"))
+if age>=18:
+    print("Eligible for license")
+else:
+    print("Not eligible")
+
+age=int(input("Enter your age:"))
+if age>=18 and age<21:
+    print("License")
+elif age>=21:
+    print("License and marriage")
+else:
+    print("Nothing")
+
+
+for i in range(0,10,1):
+    print(i)
+
+
+for i in range(0,10):
+    print(i)
+
+
+for i in range(0,10,2):
+    print(i)
+
+
+for i in range(1,50,2):
+    print(i)
+
+
+for i in range(3,31,3):
+    print(i)
+
+
+n=0
+while n<10:
+    print(n)
+    n=n+1
+
+
+n=0
+while n<6:
+    print("Hello")
+    n=n+1
+
+
+for i in range(0,6):
+    print("Hello")
+
+
+for i in range(0,5,2):
+    print(i)
+    print("Hello")
+
+
+n1=10
+while n1>5:
+    print(n1)
+    n1=n1-1
+
+#n1=10
+#while n1>5:
+    #print(n1)
+
+
+for i in range(10):
+    print(i)
+    if i>3:
+        break
+    print('Heyaa')
+
+
+for i in range(10):
+    print(i)
+    if i>3:
+        print('hi')
+        break
+
+for i in range(10):
+    if i>3:
+        print(i)
+        break
